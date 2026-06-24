d229b06db2709a0375cb869261b56f1de5f62884
Chloe <git@celosia.zip>
2026-06-24 06:08:52 -0700

    treat a logout error as a success

--- src/routes/account/logout/+page.server.ts
+++ src/routes/account/logout/+page.server.ts
@@ -1,4 +1,4 @@
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { GDPSClient } from '$lib/api';
 
@@ -18,7 +18,7 @@ export const load: PageServerLoad = async ({ cookies, fetch }) => {
 	try {
 		await client.logout();
 	} catch {
-		return error(500, 'An unknown server error has happened, please try again!');
+		return redirect(303, '/tools?logout=true');
 	}
 
 	return redirect(303, '/tools?logout=true');
