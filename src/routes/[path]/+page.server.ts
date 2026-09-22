c30e0f07b745fb900d31da9e8fb3e8f0dd26b3f1
Chloe <git@celosia.zip>
2026-09-22 17:27:52 -0500

    add link blacklist to fallback search

--- src/routes/[path]/+page.server.ts
+++ src/routes/[path]/+page.server.ts
@@ -1,10 +1,15 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { resolve } from '$app/paths';
 
 export const load: PageServerLoad = async ({ params }) => {
 	if (params.path == 'download.php') {
 		redirect(301, resolve('/download'));
+	}
+
+	const valid_level = /^[a-zA-Z0-9 -]{1,48}$/g;
+	if (!valid_level.test(params.path)) {
+		error(404);
 	}
 
 	const search = new URLSearchParams({ query: params.path });
